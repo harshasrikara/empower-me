1d47bd60fa84be3d7eb76055f24275e7b71fd2de
Steven <stevenle18@gmail.com>
2020-01-11 18:16:37 -0800

    Card test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdCard, MdButton, MdRipple } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdCard)
+Vue.use(MdButton)
+Vue.use(MdRipple)
 
 Vue.config.productionTip = false
 
